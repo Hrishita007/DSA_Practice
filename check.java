db025ab346b9db5ab38475a9b12e1d60bd3867c4
Hrishita <hrishu071@gmail.com>
2026-06-15 23:04:00 +0530

    array easy

---
+++ check.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.check(arr));
+    }
+}
