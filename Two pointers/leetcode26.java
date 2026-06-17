04ab33a71b5694456ae475e2005a9b69499ae7c3
Hrishita <hrishu071@gmail.com>
2026-06-17 23:55:34 +0530

    array

---
+++ Two pointers/leetcode26.java
@@ -0,0 +1,23 @@
+class leetcode26 {
+    public int removeDuplicates(int[] nums) {
+        int s=0,f=0;
+        for(f=0;f<nums.length;f++){
+            if(nums[f]!=nums[s]){
+                s++;
+                nums[s]=nums[f];
+            }
+        }
+        return s+1;
+        
+    }
+    public static void main(String[] args) {
+        leetcode26 solution = new leetcode26();
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int newLength = solution.removeDuplicates(nums);
+        System.out.println("New length: " + newLength);
+        System.out.print("Modified array: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
