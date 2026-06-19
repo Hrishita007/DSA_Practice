8fa817b24fef7006ef7b0a09862062028c27f432
Hrishita <hrishu071@gmail.com>
2026-06-20 00:27:01 +0530

    array medium

---
+++ Two pointers/leetcode18.java
@@ -0,0 +1,59 @@
+//4 sum
+import java.util.*;
+public class leetcode18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // Skip duplicates for the first number
+            }
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue; // Skip duplicates for the second number
+                }
+
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+
+                        // Skip duplicates for the third number
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        // Skip duplicates for the fourth number
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        leetcode18 solution = new leetcode18();
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        List<List<Integer>> result = solution.fourSum(nums, target);
+        System.out.println(result);
+    }
+}
