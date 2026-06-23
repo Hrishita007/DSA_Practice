b6c0bbaa2cd50eb4f82f2b7185df4cd4f2a3592d
Hrishita <hrishu071@gmail.com>
2026-06-23 23:42:59 +0530

    array hard

---
+++ Two pointers/leetcode42.java
@@ -0,0 +1,24 @@
+public class leetcode42 {
+    public int trap(int[] height) {
+        int l=0,r=height.length-1,res=0;
+        int maxl=height[l], maxr=height[r];
+        while(l<r){
+            if(maxl<=maxr){
+                l++;
+                maxl=Math.max(maxl,height[l]);
+                res+=maxl-height[l];
+                }
+            else{
+                r--;
+                maxr=Math.max(maxr,height[r]);
+                res+=maxr-height[r];
+            }
+        }
+         return res;
+    }
+    public static void main(String[] args) {
+        leetcode42 l=new leetcode42();
+        int[] height={0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(l.trap(height));
+    }  
+}
