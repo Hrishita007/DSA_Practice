aa203ad7d2aecb8df3fedf68db916a7b51cc7b9a
Hrishita <hrishu071@gmail.com>
2026-06-26 21:41:33 +0530

    array medium

---
+++ Arrays/Sliding Window/leetcode438.java
@@ -0,0 +1,37 @@
+//anagrams in a string
+import java.util.*;
+class leetcode438 {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+
+        int k=p.length();
+        int n=s.length();
+        if(k > n) {
+            return ans;
+        }
+        int[] target=new int[26];
+        for(char ch : p.toCharArray()) {
+            target[ch-'a']++;
+        }
+        int[] window=new int[26];
+        for(int r = 0; r < n; r++) {
+
+            window[s.charAt(r) - 'a']++;
+
+            if(r >= k) {
+
+                window[s.charAt(r - k)-'a']--;
+            }
+
+            if(r >= k - 1) {
+
+                if(Arrays.equals(target,window)) {
+                    ans.add(r-k+1);
+                }
+            }
+
+        }
+        return ans;
+    }
+}
+
