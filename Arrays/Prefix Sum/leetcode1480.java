efc59ce64c2cbb232b300e43727c5590ec0cfdd6
Hrishita <hrishu071@gmail.com>
2026-06-28 23:45:10 +0530

    array easy

---
+++ Arrays/Prefix Sum/leetcode1480.java
@@ -0,0 +1,18 @@
+//running sum of 1d array
+import java.util.Arrays;
+class leetcode1480 {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+        
+    }
+    public static void main(String[] args) {
+        leetcode1480 obj = new leetcode1480();
+        int[] nums = {1, 2, 3, 4};
+        int[] result = obj.runningSum(nums);
+        System.out.println(Arrays.toString(result)); // Output: [1, 3, 6, 10]
+    }
+}
