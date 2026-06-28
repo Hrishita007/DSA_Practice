504812898f9eafd7b208e209868e5fcbd76ca528
Hrishita <hrishu071@gmail.com>
2026-06-28 23:26:26 +0530

    array hard

---
+++ Arrays/Sliding Window/leetcode30.java
@@ -0,0 +1,61 @@
+//Substring with Concatenation of All Words
+import java.util.*;
+class leetcode30 {
+    public List<Integer> findSubstring(String s, String[] words) {
+        
+        List<Integer> ans = new ArrayList<>();
+        if (s == null || words.length == 0) return ans;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+        if (s.length() < totalLen) return ans;
+
+        Map<String, Integer> target = new HashMap<>();
+        for (String w : words) {
+            target.put(w, target.getOrDefault(w, 0) + 1);
+        }
+             
+        for (int start = 0; start < wordLen; start++) {
+
+            int left = start;
+            int count = 0;
+            Map<String, Integer> window = new HashMap<>();
+
+            for (int right = start; right + wordLen <= s.length(); right += wordLen) {
+
+                String word = s.substring(right, right + wordLen);
+
+                if (target.containsKey(word)) {
+
+                    window.put(word, window.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (window.get(word) > target.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        window.put(leftWord, window.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == wordCount) {
+                        ans.add(left);
+                    }
+
+                } else {
+                    window.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        leetcode30 obj = new leetcode30();
+        String s = "barfoothefoobarman";
+        String[] words = {"foo", "bar"};
+        System.out.println(obj.findSubstring(s, words)); // Output: [0, 9]
+    }
+}
