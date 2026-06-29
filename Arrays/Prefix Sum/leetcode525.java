43232ed3ddab77196fbbb50204aa2527984a6802
Hrishita <hrishu071@gmail.com>
2026-06-30 00:38:45 +0530

    array medium

--- Arrays/Prefix Sum/leetcode525.java
+++ Arrays/Prefix Sum/leetcode525.java
@@ -39,3 +39,36 @@ class leetcode525 {
         System.out.println(maxLength); // Output: 6
     }
 }
+
+//count subarrays with equal 0s and 1s
+// import java.util.HashMap;
+
+// class Solution {
+
+//     static int countSubarrWithEqualZeroAndOne(int nums[], int n) {
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         map.put(0, 1);
+
+//         int sum = 0;
+//         int count = 0;
+
+//         for (int i = 0; i < n; i++) {
+
+//             // Convert 0 to -1
+//             sum += (nums[i] == 0) ? -1 : 1;
+
+//             // If this prefix sum has appeared before,
+//             // all previous occurrences form valid subarrays.
+//             if (map.containsKey(sum)) {
+//                 count += map.get(sum);
+//             }
+
+//             // Increase frequency of current prefix sum
+//             map.put(sum, map.getOrDefault(sum, 0) + 1);
+//         }
+
+//         return count;
+//     }
+// }
