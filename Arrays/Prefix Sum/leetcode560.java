601abb36051f18a8cc7d46cc04bd18a5c2000b37
Hrishita <hrishu071@gmail.com>
2026-06-29 23:10:52 +0530

    array medium

---
+++ Arrays/Prefix Sum/leetcode560.java
@@ -0,0 +1,26 @@
+//find the number of subarrays with sum equal to k
+import java.util.HashMap;
+class leetcode560 {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        int prefixsum=0;
+        int result=0;
+        for(int num:nums){
+            prefixsum+=num;
+            if(map.containsKey(prefixsum-k)){
+                result+=map.get(prefixsum-k);
+            }
+            map.put(prefixsum,map.getOrDefault(prefixsum,0)+1);
+        }
+        return result;
+        
+    }
+    public static void main(String[] args) {
+        leetcode560 obj = new leetcode560();
+        int[] nums = {1, 1, 1};
+        int k = 2;
+        int result = obj.subarraySum(nums, k);
+        System.out.println(result); // Output: 2
+    }
+}
