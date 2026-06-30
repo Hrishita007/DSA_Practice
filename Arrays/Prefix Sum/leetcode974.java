8430c0af8c9b65d89240d32564f5825840c0d4aa
Hrishita <hrishu071@gmail.com>
2026-06-30 22:19:21 +0530

    array medium

--- Arrays/Prefix Sum/leetcode974.java
+++ Arrays/Prefix Sum/leetcode974.java
@@ -36,17 +36,19 @@ class leetcode974 {
 //print subarrays divisible by k
 // import java.util.*;
 
-// class Solution {
-//     public static List<List<Integer>> printSubarraysDivByK(int[] nums, int k) {
+// class leetcode974 {
+//     public int subarraysDivByK(int[] nums, int k) {
 
-//         Map<Integer, List<Integer>> map = new HashMap<>();
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         HashMap<Integer, List<Integer>> indicesMap = new HashMap<>();
 
-//         map.put(0, new ArrayList<>());
-//         map.get(0).add(-1); // prefix before array starts
+//         map.put(0, 1);
+
+//         indicesMap.put(0, new ArrayList<>());
+//         indicesMap.get(0).add(-1); // prefix start
 
 //         int sum = 0;
-
-//         List<List<Integer>> result = new ArrayList<>();
+//         int count = 0;
 
 //         for (int i = 0; i < nums.length; i++) {
 
@@ -54,28 +56,31 @@ class leetcode974 {
 
 //             int rem = ((sum % k) + k) % k;
 
-//             if (!map.containsKey(rem)) {
-//                 map.put(rem, new ArrayList<>());
+//             // ---- PRINT LOGIC ADDED ----
+//             if (indicesMap.containsKey(rem)) {
+//                 for (int start : indicesMap.get(rem)) {
+//                     System.out.println("Subarray: " + (start + 1) + " to " + i);
+//                 }
 //             }
 
-//             List<Integer> indices = map.get(rem);
+//             // ---- YOUR ORIGINAL LOGIC ----
+//             count += map.getOrDefault(rem, 0);
+//             map.put(rem, map.getOrDefault(rem, 0) + 1);
 
-//             // every previous index gives a valid subarray
-//             for (int startIdx : indices) {
-//                 result.add(build(nums, startIdx + 1, i));
-//             }
-
-//             indices.add(i);
+//             // ---- STORE INDEX FOR PRINTING ----
+//             indicesMap.putIfAbsent(rem, new ArrayList<>());
+//             indicesMap.get(rem).add(i);
 //         }
 
-//         return result;
+//         return count;
 //     }
 
-//     private static List<Integer> build(int[] nums, int l, int r) {
-//         List<Integer> sub = new ArrayList<>();
-//         for (int i = l; i <= r; i++) {
-//             sub.add(nums[i]);
-//         }
-//         return sub;
+//     public static void main(String[] args) {
+//         leetcode974 obj = new leetcode974();
+//         int[] nums = {4, 5, 0, -2, -3, 1};
+//         int k = 5;
+
+//         int result = obj.subarraysDivByK(nums, k);
+//         System.out.println("Count = " + result);
 //     }
 // }
