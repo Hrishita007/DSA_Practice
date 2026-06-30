774fba6a55d2d72fb653aa0a500041dbafba4022
Hrishita <hrishu071@gmail.com>
2026-06-30 22:06:35 +0530

    array medium

--- Arrays/Prefix Sum/leetcode974.java
+++ Arrays/Prefix Sum/leetcode974.java
@@ -32,3 +32,50 @@ class leetcode974 {
         System.out.println(result); // Output: 7
     }
 }
+
+//print subarrays divisible by k
+// import java.util.*;
+
+// class Solution {
+//     public static List<List<Integer>> printSubarraysDivByK(int[] nums, int k) {
+
+//         Map<Integer, List<Integer>> map = new HashMap<>();
+
+//         map.put(0, new ArrayList<>());
+//         map.get(0).add(-1); // prefix before array starts
+
+//         int sum = 0;
+
+//         List<List<Integer>> result = new ArrayList<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+
+//             sum += nums[i];
+
+//             int rem = ((sum % k) + k) % k;
+
+//             if (!map.containsKey(rem)) {
+//                 map.put(rem, new ArrayList<>());
+//             }
+
+//             List<Integer> indices = map.get(rem);
+
+//             // every previous index gives a valid subarray
+//             for (int startIdx : indices) {
+//                 result.add(build(nums, startIdx + 1, i));
+//             }
+
+//             indices.add(i);
+//         }
+
+//         return result;
+//     }
+
+//     private static List<Integer> build(int[] nums, int l, int r) {
+//         List<Integer> sub = new ArrayList<>();
+//         for (int i = l; i <= r; i++) {
+//             sub.add(nums[i]);
+//         }
+//         return sub;
+//     }
+// }
