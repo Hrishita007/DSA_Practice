e229c11d3e2c8c772dc146653e749cf20d13f39d
Hrishita <hrishu071@gmail.com>
2026-07-02 22:20:55 +0530

    array medium

--- Arrays/Kadane's Algorithm/leetcode152.java
+++ Arrays/Kadane's Algorithm/leetcode152.java
@@ -28,3 +28,94 @@ class leetcode152 {
         System.out.println("Maximum Product Subarray: " + maxProduct); // Output: 6
     }
 }
+
+
+
+// import java.util.Arrays;
+
+// // Maximum Product Subarray print subarray
+// class leetcode152 {
+
+//     public int maxProduct(int[] nums) {
+
+//         int current = nums[0];
+//         int minProduct = nums[0];
+//         int maxProduct = nums[0];
+
+//         int currentStart = 0;
+//         int minStart = 0;
+
+//         int ansStart = 0;
+//         int ansEnd = 0;
+
+//         for (int i = 1; i < nums.length; i++) {
+
+//             int oldCurrent = current;
+//             int oldMin = minProduct;
+
+//             int oldCurrentStart = currentStart;
+//             int oldMinStart = minStart;
+
+//             // Compute current max
+//             if (nums[i] >= oldCurrent * nums[i] &&
+//                 nums[i] >= oldMin * nums[i]) {
+
+//                 current = nums[i];
+//                 currentStart = i;
+
+//             } else if (oldCurrent * nums[i] >= oldMin * nums[i]) {
+
+//                 current = oldCurrent * nums[i];
+//                 currentStart = oldCurrentStart;
+
+//             } else {
+
+//                 current = oldMin * nums[i];
+//                 currentStart = oldMinStart;
+//             }
+
+//             // Compute current min
+//             if (nums[i] <= oldCurrent * nums[i] &&
+//                 nums[i] <= oldMin * nums[i]) {
+
+//                 minProduct = nums[i];
+//                 minStart = i;
+
+//             } else if (oldCurrent * nums[i] <= oldMin * nums[i]) {
+
+//                 minProduct = oldCurrent * nums[i];
+//                 minStart = oldCurrentStart;
+
+//             } else {
+
+//                 minProduct = oldMin * nums[i];
+//                 minStart = oldMinStart;
+//             }
+
+//             if (current > maxProduct) {
+//                 maxProduct = current;
+//                 ansStart = currentStart;
+//                 ansEnd = i;
+//             }
+//         }
+
+//         System.out.print("Maximum Product Subarray: ");
+//         for (int i = ansStart; i <= ansEnd; i++) {
+//             System.out.print(nums[i] + " ");
+//         }
+//         System.out.println();
+
+//         return maxProduct;
+//     }
+
+//     public static void main(String[] args) {
+
+//         leetcode152 solution = new leetcode152();
+
+//         int[] nums = {-2,3,-4};
+
+//         int ans = solution.maxProduct(nums);
+
+//         System.out.println("Maximum Product: " + ans);
+//     }
+// }
--- Arrays/Kadane's Algorithm/leetcode53.java
+++ Arrays/Kadane's Algorithm/leetcode53.java
@@ -16,3 +16,54 @@ class leetcode53 {
         System.out.println("Maximum Subarray Sum: " + maxSum); // Output: 6
     }
 }
+
+// import java.util.Arrays;
+
+// // Maximum Subarray Sum print subarray
+// class leetcode53 {
+
+//     public int maxSubArray(int[] nums) {
+
+//         int current = nums[0];
+//         int maxSum = nums[0];
+
+//         int start = 0;
+//         int ansStart = 0;
+//         int ansEnd = 0;
+
+//         for (int i = 1; i < nums.length; i++) {
+
+//             if (nums[i] > current + nums[i]) {
+//                 current = nums[i];
+//                 start = i;
+//             } else {
+//                 current += nums[i];
+//             }
+
+//             if (current > maxSum) {
+//                 maxSum = current;
+//                 ansStart = start;
+//                 ansEnd = i;
+//             }
+//         }
+
+//         System.out.print("Maximum Subarray: ");
+//         for (int i = ansStart; i <= ansEnd; i++) {
+//             System.out.print(nums[i] + " ");
+//         }
+//         System.out.println();
+
+//         return maxSum;
+//     }
+
+//     public static void main(String[] args) {
+
+//         leetcode53 solution = new leetcode53();
+
+//         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+//         int maxSum = solution.maxSubArray(nums);
+
+//         System.out.println("Maximum Subarray Sum: " + maxSum);
+//     }
+// }
