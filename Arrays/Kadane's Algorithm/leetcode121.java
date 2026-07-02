54e43acd3c4978c35c1b0c76b600419a0109b384
Hrishita <hrishu071@gmail.com>
2026-07-02 22:48:16 +0530

    array easy

---
+++ Arrays/Kadane's Algorithm/leetcode121.java
@@ -0,0 +1,19 @@
+//Best time to buy and sell stock(easy)
+class leetcode121 {
+    public int maxProfit(int[] prices) {
+        int minbuy=prices[0];
+        int maxProfit=0;
+        for(int i=1;i<prices.length;i++){
+            minbuy=Math.min(minbuy,prices[i]);
+            int profit=prices[i]-minbuy;
+            maxProfit=Math.max(maxProfit,profit);
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        leetcode121 solution = new leetcode121();
+        int[] prices = {7,1,5,3,6,4};
+        int maxProfit = solution.maxProfit(prices);
+        System.out.println("Maximum Profit: " + maxProfit); // Output: 5
+    }
+}
