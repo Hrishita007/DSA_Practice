b65b99ed443538fae5497f4562528a52ac8d9d8c
Hrishita <hrishu071@gmail.com>
2026-07-03 21:23:54 +0530

    array medium

--- Arrays/Kadane's Algorithm/leetcode122.java
+++ Arrays/Kadane's Algorithm/leetcode122.java
@@ -1,5 +1,5 @@
 //Best time to buy and sell stock II(medium)
-class eetcode122 {
+class leetcode122 {
     public int maxProfit(int[] prices) {
         int profit=0;
         for(int i=1;i<prices.length;i++){
@@ -10,7 +10,7 @@ class eetcode122 {
         return profit;
     }
     public static void main(String[] args) {
-        eetcode122 solution = new eetcode122();
+        leetcode122 solution = new leetcode122();
         int[] prices = {7,1,5,3,6,4};
         int maxProfit = solution.maxProfit(prices);
         System.out.println("Maximum Profit: " + maxProfit); // Output: 7
