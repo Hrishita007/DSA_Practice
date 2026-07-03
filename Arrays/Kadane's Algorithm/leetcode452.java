510dbd4b80b7d09880f4a5e61aac482c4b711cbe
Hrishita <hrishu071@gmail.com>
2026-07-04 01:18:55 +0530

    array medium

---
+++ Arrays/Kadane's Algorithm/leetcode452.java
@@ -0,0 +1,27 @@
+// Minimum Number of Arrows to Burst Balloons
+
+import java.util.Arrays;
+
+class leetcode452 {
+    public int findMinArrowShots(int[][] points) {
+
+        if (points.length == 0) return 0;
+
+        // sort by end
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int arrows = 1;
+        int arrowPos = points[0][1];
+
+        for (int i = 1; i < points.length; i++) {
+
+            // if current balloon starts after arrow position → new arrow
+            if (points[i][0] > arrowPos) {
+                arrows++;
+                arrowPos = points[i][1];
+            }
+        }
+
+        return arrows;
+    }
+}
