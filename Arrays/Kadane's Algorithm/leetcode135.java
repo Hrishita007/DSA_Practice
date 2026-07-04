738d2d0b2134395365e72509d64bdf0e286c03b4
Hrishita <hrishu071@gmail.com>
2026-07-04 23:32:35 +0530

    array medium

---
+++ Arrays/Kadane's Algorithm/leetcode135.java
@@ -0,0 +1,27 @@
+//Candies
+import java.util.Arrays;
+class Solution {
+    public int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i] > ratings[i-1])
+                candies[i]=candies[i-1]+1;
+        }
+        for(int i=ratings.length-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1])
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+        int total=0;
+        for(int candy:candies){
+            total+=candy;
+        }
+        return total;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] ratings = {1, 0, 2};
+        int result = solution.candy(ratings);
+        System.out.println("Minimum candies needed: " + result); // Output: 5
+    }
+}
