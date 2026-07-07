1aa09b2511aa3878dd284d9602f8ec9f6d6ffca0
Hrishita <hrishu071@gmail.com>
2026-07-07 21:52:50 +0530

    array medium

---
+++ Arrays/Searching & Sorting/leetcode435.java
@@ -0,0 +1,27 @@
+//Non-overlapping Intervals
+import java.util.*;
+class leetcode435 {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int remove=0;
+        int[] current = intervals[0];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<current[1]){
+                remove++;
+            }
+            else{
+                current= intervals[i];
+            }
+        }
+        return remove;
+    }
+    public static void main(String[] args) {
+        leetcode435 solution = new leetcode435();
+
+        int[][] intervals = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
+
+        int result = solution.eraseOverlapIntervals(intervals);
+
+        System.out.println("Minimum number of intervals to remove: " + result);
+    }
+}
