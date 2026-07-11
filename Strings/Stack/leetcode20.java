c0b5c7bcddbe61ddcb01775c86af460e9f5b0c73
Hrishita <hrishu071@gmail.com>
2026-07-11 22:55:16 +0530

    string easy

---
+++ Strings/Stack/leetcode20.java
@@ -0,0 +1,32 @@
+//valid parentheses
+//time complexity: O(n)
+//space complexity: O(n)
+import java.util.*;
+class leetcode20 {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='[')
+                st.push(ch);
+            else{
+                if(st.isEmpty())
+                    return false;
+                else if(ch==')' && st.peek()=='(')
+                    st.pop();
+                else if(ch=='}' && st.peek()=='{')
+                    st.pop();
+                else if(ch==']' && st.peek()=='[')
+                    st.pop();
+                else 
+                    return false;
+            } 
+        }
+        return st.isEmpty();
+        
+    }
+    public static void main(String[] args) {
+        leetcode20 obj=new leetcode20();
+        String s="()[]{}";
+        System.out.println(obj.isValid(s));
+    }
+}
