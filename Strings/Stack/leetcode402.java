001541d3ea6d910d990e0bbf0ba5e20262cd6094
Hrishita <hrishu071@gmail.com>
2026-07-13 23:57:19 +0530

    string medium

---
+++ Strings/Stack/leetcode402.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : num.toCharArray()) {
+
+            while (!stack.isEmpty() &&
+                   k > 0 &&
+                   ch < stack.peek()) {
+
+                stack.pop();
+                k--;
+            }
+
+            stack.push(ch);
+        }
+
+        // Remove remaining digits from the end
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+
+        // Build the answer
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        sb.reverse();
+
+        // Remove leading zeros
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+
+        // Edge case: everything removed
+        if (sb.length() == 0) {
+            return "0";
+        }
+
+        return sb.toString();
+    }
+}
