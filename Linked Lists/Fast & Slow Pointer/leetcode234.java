6527357bdb0704b0569ae5ee8ef54bce5e00ad03
Hrishita <hrishu071@gmail.com>
2026-07-18 02:02:24 +0530

    LL easy

--- Linked Lists/Fast & Slow Pointer/leetcode234.java
+++ Linked Lists/Fast & Slow Pointer/leetcode234.java
@@ -70,3 +70,54 @@ class leetcode234 {
         System.out.println("Is Palindrome: " + isPalindrome);
     }
 }
+
+//edge case: 1->2->3->2->1
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+
+//         if (head == null || head.next == null)
+//             return true;
+
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         while (fast.next != null && fast.next.next != null) {
+
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+
+//         ListNode secondHalf = reverse(slow.next);
+
+//         ListNode first = head;
+//         ListNode second = secondHalf;
+
+//         while (second != null) {
+
+//             if (first.val != second.val)
+//                 return false;
+
+//             first = first.next;
+//             second = second.next;
+//         }
+
+//         return true;
+//     }
+
+
+//     private ListNode reverse(ListNode head) {
+
+//         ListNode prev = null;
+//         ListNode curr = head;
+
+//         while (curr != null) {
+
+//             ListNode next = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = next;
+//         }
+
+//         return prev;
+//     }
+// }
