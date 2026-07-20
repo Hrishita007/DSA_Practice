d02d5f1598e718160fc22387940a6eddc3443f38
Hrishita <hrishu071@gmail.com>
2026-07-20 22:50:48 +0530

    LL medium

--- Linked Lists/Two Pointers/leetcode19.java
+++ Linked Lists/Two Pointers/leetcode19.java
@@ -52,3 +52,49 @@ class leetcode19 {
         }
     }
 }
+
+
+// //using one while loop
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+
+//         ListNode slow = dummy;
+//         ListNode fast = dummy;
+
+//         int count = 0;
+
+//         while (fast != null) {
+
+//             fast = fast.next;
+//             count++;
+
+//             if (count > n + 1) {
+//                 slow = slow.next;
+//             }
+//         }
+
+//         slow.next = slow.next.next;
+
+//         return dummy.next;
+//     }
+//     public static void main(String[] args) {
+//         Solution obj = new Solution();
+//         ListNode head = new ListNode(1);
+//         head.next = new ListNode(2);
+//         head.next.next = new ListNode(3);
+//         head.next.next.next = new ListNode(4);
+//         head.next.next.next.next = new ListNode(5);
+
+//         int n = 2;
+//         ListNode updatedList = obj.removeNthFromEnd(head, n);
+
+//         // Print the updated list
+//         while (updatedList != null) {
+//             System.out.print(updatedList.val + " ");
+//             updatedList = updatedList.next;
+//         }
+//     }
+// }
