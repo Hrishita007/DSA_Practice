326969b135231ac9a809cd08fdb13fc26d4aef2c
Hrishita <hrishu071@gmail.com>
2026-07-24 23:55:35 +0530

    array medium

--- Linked Lists/HashMap/leetcode138.java
+++ Linked Lists/HashMap/leetcode138.java
@@ -1,17 +1,18 @@
-/**
- * Definition for a Node.
- * class Node {
- *     int val;
- *     Node next;
- *     Node random;
- *
- *     public Node(int val) {
- *         this.val = val;
- *         this.next = null;
- *         this.random = null;
- *     }
- * }
- */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution {
     public Node copyRandomList(Node head) {
