afe7de91f07ff76867355b7f8f7c2323f45788ff
Hrishita <hrishu071@gmail.com>
2026-07-28 23:55:37 +0530

    sorting

---
+++ Sorting Algorithms/BucketSort.java
@@ -0,0 +1,8 @@
+//Bucket Sort is a sorting algorithm that distributes elements into a number of buckets.
+//  Each bucket is then sorted individually, either using a different sorting algorithm or 
+// by recursively applying the bucket sort algorithm.
+//time complexity: O(n + k) where n is the number of elements and k is the number of buckets
+//space complexity: O(n + k) where n is the number of elements and k is
+public class BucketSort {
+    
+}
