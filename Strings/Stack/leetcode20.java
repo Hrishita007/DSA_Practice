46cb55c490520627bdbd6ad8bf889e836778587f
Hrishita <hrishu071@gmail.com>
2026-07-31 23:39:23 +0530

    Modifications

--- Strings/Stack/leetcode20.java
+++ Strings/Stack/leetcode20.java
@@ -30,3 +30,25 @@ class leetcode20 {
         System.out.println(obj.isValid(s));
     }
 }
+
+// //another way
+// public boolean isValid(String s) {
+//     Stack<Character> st = new Stack<>();
+
+//     for (char c : s.toCharArray()) {
+//         if (c == '(') {
+//             st.push(')');
+//         } else if (c == '{') {
+//             st.push('}');
+//         } else if (c == '[') {
+//             st.push(']');
+//         } else {
+//             if (st.isEmpty() || st.pop() != c) {
+//                 return false;
+//             }
+//         }
+//     }
+
+//     return st.isEmpty();
+// }
+ 
