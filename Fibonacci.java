d0690e0746e81fd3e99d192bd1d2b4b14e6a63ef
Hrishita <hrishu071@gmail.com>
2026-08-01 19:32:21 +0530

    Fibonacci with  DP

---
+++ Fibonacci.java
@@ -0,0 +1,61 @@
+//fibonacci series using recursion
+// import java.util.Scanner;
+
+// public class Fibonacci {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the number of terms: ");
+//         int n = sc.nextInt();
+//         System.out.println("Fibonacci series:");
+//         for (int i = 0; i < n; i++) {
+//             System.out.print(fibonacci(i) + " ");
+//         }
+//         //System.out.println(fibonacci(63));
+//         //print the nth fibonacci number
+//         System.out.print("\nEnter the position of the Fibonacci number to find: ");
+//         int p = sc.nextInt();
+//         System.out.println("The Fibonacci number at position " + p + " is: " + fibonacci(p));
+
+        
+
+//     }
+
+//     public static int fibonacci(int n) {
+//         if (n <= 1) {
+//             return n;
+//         }
+//         return fibonacci(n - 1) + fibonacci(n - 2);
+//     }
+// }
+
+//fibonacci series using dynamic programming
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        long n = sc.nextLong();
+        System.out.println("Fibonacci series:");
+        for (long i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+        //print the nth fibonacci number
+        System.out.print("\nEnter the position of the Fibonacci number to find: ");
+        long p = sc.nextLong();
+        System.out.println("The Fibonacci number at position " + p + " is: " + fibonacci(p));
+    }
+
+    public static long fibonacci(long n) {
+        if (n <= 1) {
+            return n;
+        }
+        long[] fib = new long[(int) n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (long i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
+    }
+}
+
