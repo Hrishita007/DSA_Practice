199d59d6bb09341d6ccfce0c0da2f35317c040b6
Hrishita <hrishu071@gmail.com>
2026-08-08 22:29:31 +0530

    DS imp

--- Arrays/Kadane's Algorithm/leetcode121.java
+++ Arrays/Kadane's Algorithm/leetcode121.java
@@ -18,3 +18,30 @@ class leetcode121 {
         System.out.println("Maximum Profit: " + maxProfit); // Output: 5
     }
 }
+
+
+//using dynamic programming
+// class leetcode121 {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         if (n == 0) return 0;
+
+//         int[] dp = new int[n];
+//         dp[0] = 0; // No profit on the first day
+//         int minPrice = prices[0];
+
+//         for (int i = 1; i < n; i++) {
+//             minPrice = Math.min(minPrice, prices[i]);
+//             dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);
+//         }
+
+//         return dp[n - 1];
+//     }
+
+//     public static void main(String[] args) {
+//         leetcode121 solution = new leetcode121();
+//         int[] prices = {7,1,5,3,6,4};
+//         int maxProfit = solution.maxProfit(prices);
+//         System.out.println("Maximum Profit: " + maxProfit); // Output: 5
+//     }
+// }
