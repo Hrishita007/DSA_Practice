35af08bee6d3b3743a8001171ab762dd08d94566
Hrishita <hrishu071@gmail.com>
2026-08-13 23:36:19 +0530

    DS imp

--- Strings/RevString.java
+++ Strings/RevString.java
@@ -18,3 +18,36 @@ public class RevString {
 //String s = "hello";
 //String reversed = reverseString(s);
 //System.out.println(reversed); // Output: "olleh"
+
+//Reverse without using Reverse method
+// public class RevString {
+//     public static void main(String[] args) {
+//         String s = "hello";
+//         String reversed = reverseString(s);
+//         System.out.println(reversed); // Output: "olleh"
+//     }
+
+//     public static String reverseString(String s) {
+//         char[] charArray = s.toCharArray();
+//         int left = 0;
+//         int right = charArray.length - 1;
+
+//         while (left < right) {
+//             // Swap characters
+//             char temp = charArray[left];
+//             charArray[left] = charArray[right];
+//             charArray[right] = temp;
+
+//             left++;
+//             right--;
+//         }
+
+//         return new String(charArray);
+//     }
+//     public static String reverseStringRecursively(String s) {
+//         if (s.isEmpty()) {
+//             return s;
+//         }
+//         return reverseStringRecursively(s.substring(1)) + s.charAt(0);
+//     }
+// }
